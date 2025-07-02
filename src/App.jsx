030bbafefc235e1c5e34ed799fda3b5a6a3161a9
daru121 <darucaraka43@gmail.com>
2025-07-02 10:14:09 +0800

    Add footer credit to App component

--- src/App.jsx
+++ src/App.jsx
@@ -541,6 +541,10 @@ function App() {
               </div>
             </div>
           </div>
+          {/* Footer Credit */}
+          <div className="mt-4 text-center text-gray-600 text-sm">
+            <p>Created with by Daru Caraka</p>
+          </div>
         </div>
       </div>
 
